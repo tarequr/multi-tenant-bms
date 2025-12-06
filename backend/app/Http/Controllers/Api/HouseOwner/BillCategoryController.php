b1fdd31fd1b381fc31ace4a33d80095cf2e4bb2d
tarequr <tarequrr8@gmail.com>
2025-12-06 22:01:12 +0600

    feat: created all controller and middleware

---
+++ backend/app/Http/Controllers/Api/HouseOwner/BillCategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\HouseOwner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BillCategoryController extends Controller
+{
+    //
+}
